cf50302d0a375502cb172cb1dfcde7abdae9c54b
JP Dillingham <jpdillingham@gmail.com>
2017-05-10 00:50:40 -0700

    Changed access modifier for program/main

--- lets-code-ci-session-2/Program.cs
+++ lets-code-ci-session-2/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace lets_code_ci_session_2
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
     }
